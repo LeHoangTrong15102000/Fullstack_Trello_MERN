49adc6afda464526d0199bb683ea09ed263cf1f2
Le Hoang Trong <langtupro0456@gmail.com>
2023-12-30 01:11:51 +0700

    Fix BUILD_MODE dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,14 @@
 // Chứa các constants
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-qlap.onrender.com'
+}
+// console.log('🚀 ~ file: constants.js:3 ~ apiRoot:', apiRoot)
+
+export const API_ROOT = apiRoot
 // export const API_ROOT = 'https://trello-api-qlap.onrender.com'
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cho phép thầng vite sử dụng được process.env, mặc đinh phải dùng import.meta.env
+  define: {
+    'process.env': process.env
+  },
   plugins: [react(), svgr()],
   server: {
     port: 3000
