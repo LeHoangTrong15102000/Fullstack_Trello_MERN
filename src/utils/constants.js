2aca4c79e6e1cfa1acb3d2ce5e6a0de8f90b4ef5
Le Hoang Trong <langtupro0456@gmail.com>
2023-12-30 00:42:14 +0700

    Deploy node engine 18.x to Vercel

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 // Chứa các constants
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-api-qlap.onrender.com'
+export const API_ROOT = 'http://localhost:8017'
+// export const API_ROOT = 'https://trello-api-qlap.onrender.com'
